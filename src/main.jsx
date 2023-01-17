807965ccfb444e4ad6b2bafffb25345510480ddb
Florian Pasquet <florianpasquett@gmail.com>
2023-01-17 09:26:31 +0100

    change router for electron 🚧

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom';
 
 //Style Import
 import './assets/css/bootstrap.css';
@@ -24,7 +24,7 @@ import Account from './routes/account';
 
 
 //Router
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
